fd44c80d9725924a4cbf4fcd4bc06e625d1e4ae6
Marek Gozdalski <marek.gozdalski@gmail.com>
2021-09-28 16:11:21 +0200

    dbme.w.lib.core

--- types/dbme.w.lib.core.d.ts
+++ types/dbme.w.lib.core.d.ts
@@ -1,5 +1,73 @@
-declare module "dbme/w/lib/core" {
-    export namespace dbme.w.lib.core {}
+declare namespace dbme.w.lib.core {
+
+    export enum Entityset {
+        App = "CMNAppSet",
+        AbsenceType = "CMNAbsenceTypeSet",
+        AddressTitle = "CMNAddrTitleSet",
+        AllocationInterval = "WPLIntervalSet",
+        AllocationIntervalCreate = "WPLIntervalCreateSet",
+        AllocationIntervalText = "WPLIntervalTextSet",
+        CalendarViews = "CMNCalendarViewSet",
+        Country = "CMNCountrySet",
+        Demand = "WPLDemandSet",
+        DemandCreate = "WPLDemandCreateSet",
+        DemandRoles = "WPLDemandDDRolesSet",
+        DemandType = "WPLDemandTypeSet",
+        UIFilters = "CMNFiltersSet",
+        HierarchyPath = "HASNodePathSet",
+        Interval = "RPLIntervalSet",
+        IntervalCreate = "RPLIntervalCreateSet",
+        IntervalType = "CMNIntervalTypeSet",
+        MakeCode = "CMNMakeSet",
+        MileageUom = "CMNMileageUomSet",
+        MimeTypes = "CMNMimeTypeSet",
+        ModelCode = "CMNModelSet",
+        OrderType = "CMNOrderTypeSet",
+        PlanningMode = "WPLPlanningModeSet",
+        Region = "CMNCountryRegionSet",
+        Resource = "CMNResourceSet",
+        ResourceAssignment = "HASAssignmentSet",
+        ResourceAvatar = "CMNResourceAvatarSet",
+        ResourceStatus = "CMNResourceStatusSet",
+        ResourceRole = "CMNResourceRoleSet",
+        ResourceType = "CMNResourceTypeSet",
+        ReservationType = "CMNReservationTypeSet",
+        TimeStep = "CMNTimeStepSet",
+        TreeNode = "HASTreeNodeSet",
+        TreeNodeCreate = "HASTreeNodeCreateSet",
+        TodoAllocationProposal = "TDOAllocationProposalSet",
+        TodoDecTreeItem = "TDODecisionTreeItemSet",
+        TodoLocation = "TDOLocationSet",
+        TodoLocationGroup = "TDOLocationGroupSet",
+        TodoOrder = "TDOOrderSet",
+        TodoOrderList = "TDOOrderListSet",
+        TodoOrderAction = "TDOActionSet",
+        TodoOrderAgreement = "TDOOrderAgreementSet",
+        TodoOrderAttachment = "TDOOrderAttachmentSet",
+        TodoOrderCommChannel = "TDOCommChannel",
+        TodoOrderDemand = "TDODemandSet",
+        TodoOrderFields = "TDOScreenFieldSet",
+        TodoOrderHistory = "TDOOrderHistorySet",
+        TodoOrderNote = "TDOOrderNoteSet",
+        TodoOrderParameter = "TDOOrderParameterSet",
+        TodoVehiCust = "TDOVehicleCustomerSet",
+        TodoVehiCustSH = "TDOVehicleCustomerMDSet",
+        TodoResource = "TDOResourceSet",
+        TodoRole = "TDORoleSet",
+        TodoStatus = "TDOStatusSet",
+        TodoStatusAction = "TDOStatusActionSet", //readonly
+        TodoScreens = "TDOServiceScreenSet",
+        TodoServiceSection = "TDOServiceSectionSet",
+        TodoServiceItem = "TDOServiceItemSet",
+        TodoTimeSlot = "TDOTimeSlotsSet",
+        TodoUser = "TDOUserSet",
+        TodoWorkItem = "TDOWorkItemSet",
+        TodoWorkshop = "TDOWorkshopSet",
+        UIAppSettings = "CMNUISettingsSet", //LC based, app. backend configuration synchronousely read in Component.init()
+        UserHierarchyAsgn = "CMNHierarchyAsgnSet", //user hierarchy assignment
+        WorkingModel = "CMNWorkingModelSet",
+        WorkingHours = "CMNWorkingHoursSet"
+    }
 }
 
 declare module "dbme/w/lib/core/library" {
@@ -15,5 +83,7 @@ declare module "dbme/w/lib/core/ui/UIComponent" {
 
     export default class UIComponent extends UICom {
         public static getDBMEComponent(): string;
+        public getCurrentRouteName(): string;
+        public setCurrentRouteName(name: string): void;
     }
 }
