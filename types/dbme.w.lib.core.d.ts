315e7502d6e229b77f6750e72002bf897961edd6
Marek Gozdalski <marek.gozdalski@gmail.com>
2021-09-28 15:36:46 +0200

    dbme/w/lib/core/ui/UIComponent

--- types/dbme.w.lib.core.d.ts
+++ types/dbme.w.lib.core.d.ts
@@ -1,1 +1,12 @@
-declare module "dbme/w/lib/core/library" {}
+import ResourceBundle from "sap/base/i18n/ResourceBundle";
+import { default as UICom } from "sap/ui/core/UIComponent";
+
+declare module "dbme/w/lib/core" {
+    export class library {
+        public static getResourceBundle(): ResourceBundle;
+    }
+}
+
+declare module "dbme/w/lib/core/ui" {
+    export class UIComponent extends UICom {}
+}
