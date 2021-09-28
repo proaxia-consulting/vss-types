faa8e5afcd7667dda4533bd104c04501c5df3a2e
Marek Gozdalski <marek.gozdalski@gmail.com>
2021-09-28 17:21:39 +0200

    dbme/c/controller/Base, dbme/w/lib/core/controller/Base

--- types/dbme.c.d.ts
+++ types/dbme.c.d.ts
@@ -1,11 +1,36 @@
 declare module "dbme/c" {
-    export namespace dbme.c {}
+    namespace dbme.c {
+        export enum HttpHeaderName {
+			DBMEMessage = "dbme-message"
+		}
+    }
+    export default dbme.c;
 }
 
 declare module "dbme/c/library" {
     import ResourceBundle from "sap/base/i18n/ResourceBundle";
-    
+
     export default class library {
         public static getResourceBundle(): ResourceBundle;
+        public static getVersion(): number;
     }    
+}
+
+declare module "dbme/c/Log" {
+    export default class Log {
+        public addResponse(oResponse: object, oDefaultSuccess: any, oDefaultError: any): any;
+        public hasError(oResponse: object): boolean;
+    }
+}
+
+declare module "dbme/c/controller/Base" {
+    import Controller from "sap/ui/core/mvc/Controller";
+    import ODataModel from "sap/ui/model/odata/v2/ODataModel";
+    import Log from "dbme/c/Log";
+
+    export default abstract class Base extends Controller {
+        public abstract _getModel(): ODataModel;
+        public getLog(): Log;
+        public _(key: string, args?: any): string;
+    }
 }
--- types/dbme.w.lib.core.d.ts
+++ types/dbme.w.lib.core.d.ts
@@ -105,3 +105,33 @@ declare module "dbme/w/lib/core/ui/UIComponent" {
         public setCurrentRouteName(name: string): void;
     }
 }
+
+declare module "dbme/w/lib/core/controller/Base" {
+    import AbstractBase from "dbme/c/controller/Base";
+    import ODataModel from "sap/ui/model/odata/v2/ODataModel";
+    import JSONModel from "sap/ui/model/json/JSONModel";
+    import Dialog from "sap/m/Dialog";
+    import Event from "sap/ui/base/Event";
+
+    export default abstract class Base extends AbstractBase {
+        public abstract ODataToJSON(): void;
+        public onDialogCancel(oDialog: Dialog, bAllPaths?: boolean): boolean;
+        public onPageNavBack(oEvent: Event): void;
+        public onPageNavForward(oEvent: Event): void;
+        public onFacetFilterReset(oEvent: Event): void;
+        public onShowUserSettings(oEvent: Event);
+        public getHASModel(): JSONModel;
+        public onRootHierarchyAssignmentChange(oEvent: Event): void;
+        public _getModel(): ODataModel;
+        protected _getJSONModel(oData?: any, sModelName?: string): JSONModel;
+        protected _getDialogFormData(oDialog: Dialog, bEnabled: boolean, bVisible: boolean): any;
+        protected _validateDialogForm(oDialog: Dialog): boolean;
+        protected _loadFiltersFromOData(sPath?: string): Array<Promise<any>>;
+        protected _ODataToJSON(sRemotePath: string, bForce?: boolean, aFilters?: Array<any>, oTargetModel?: JSONModel): Promise<any>;
+        protected _getJSONDataPromises(): Array<Promise<any>>;
+        protected _jsonDataLoaded(): Promise<JSONModel>;
+        protected _getConfirmDialog(confirm: CallableFunction, aArguments?: Array<any>): Dialog;
+        protected _loadUISettings(): Array<any> | Promise<any>;
+        protected _getKnownFields(sEntityset: string): Array<string>;        
+    }
+}
