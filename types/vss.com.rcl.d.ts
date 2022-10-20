96eeccdab9e7c6be286c923969e4ecf766514103
Marek Gozdalski <marek.gozdalski@gmail.com>
2022-10-20 17:05:19 +0200

    vss.com.rcl

--- types/vss.com.rcl.d.ts
+++ types/vss.com.rcl.d.ts
@@ -1,10 +1,197 @@
+declare module "vss/com/rcl/cc/model/Enums" {
+	export enum FixedString {
+		eol = "\n\r"
+	}
+	export enum ModelName {
+		action = "action",
+		cart = "cart",
+		device = "device",
+		i18n = "i18n",
+		local = "local",
+		ui = "ui",
+		pageInternal = "pageInternal",
+		internal = "internal"
+	}
+	export enum RouteName {
+		ContractList = "ContractList",
+		ContractObjectPage = "ContractObjectPage",
+		ContractItemObjectPageOV = "ContractItemObjectPageOV",
+		ContractItemObjectPageLG = "ContractItemObjectPageLG",
+		ContractItemObjectPagePG = "ContractItemObjectPagePG",
+		ContractItemObjectPagePR = "ContractItemObjectPagePR"
+	}
+	export enum DataSourceName {
+		mainService = "mainService"
+	}
+	export enum ControlId {
+		/**
+		 * @link https://sapui5.hana.ondemand.com/1.96.1/resources/sap/fe/templates/ObjectPage/view/fragments/FooterContent.fragment.xml
+		 */
+		cancelButton = "fe::FooterBar::StandardAction::Cancel",
+		saveButton = "fe::FooterBar::StandardAction::Save",
+		contractItemsTable = "fe::table::_Items::LineItem"
+	}
+	export enum VSSEventId {
+		BillingCancel = "BILLING_CANCEL",
+		BillingCreate = "BILLING_CREATE"
+	}
+	export enum HTTPMethod {
+		GET = "GET",
+		PUT = "PUT",
+		POST = "POST",
+		DELETE = "DELETE"
+	}
+	export enum EditMode {
+		Editable = "Editable"
+	}
+	export enum RAPAction {
+		Prepare = "com.sap.gateway.srvd.pacg.rso_cm.v0001.Prepare"
+	}
+	export enum EntitySet {
+		ItemGroup = "ItemGroup",
+		ItemGroupText = "ItemGroupText",
+		ItemGroupCategory = "ItemGroupCategory"
+	}
+	export enum UnitOfMeasure {
+		Piece = "PC"
+	}
+}
+declare module "vss/com/rcl/types/EntitySet" {
+	import { CharacteristicDataType, RentalClassType } from "vss/com/rcl/library";
+	export type TRootEntity = {
+		Equipment: string;
+		EquipmentName: string;
+		Material: string;
+		MaterialName: string;
+		Plant: string;
+		PlantName: string;
+		StorageLocation: string;
+		StorageLocationName: string;
+		RentalClassType: string;
+		RentalClassTypeText: string;
+		HierarchyLevel: number;
+		HierarchyRoot: string;
+		SuperordinateEquipment: string;
+		SuperordinateEquipmentName: string;
+		_CharacteristicsDisplay?: TObjectCharacteristicsDisplay[];
+	};
+	export type TObjectCharacteristicsDisplay = {
+		ClfnObjectID: string;
+		ClfnObjectTable: string;
+		ClassInternalID: string;
+		CharcInternalID: string;
+		SequenceNumber: number;
+		Icon: string;
+		CharcDescription: string;
+		_Value?: TCMObjectCharcValueEntity[];
+	};
+	export type TCMObjectCharcValueEntity = {
+		ClfnObjectID: string;
+		ClfnObjectTable: string;
+		CharcInternalID: string;
+		CharcValue: string;
+		CharcValueDescription?: string;
+		CharcFromDecimalValue: string;
+		_Characteristic?: TCharacteristic;
+		_FixedValue?: TCharcValEntity;
+	};
+	export type TCharacteristic = {
+		CharcInternalID: string;
+		CharcDecimals: number;
+		CharcLength: number;
+	};
+	export type TCharcFilterEntity = {
+		CharcInternalID: string;
+		_Charc?: TCharcEntity;
+		_ClassCharc?: TClassCharcEntity[];
+	};
+	export type TClassCharcEntity = {
+		CharcInternalID: string;
+		ClassInternalID: string;
+		ClassDescription: string;
+		RentalClassType: RentalClassType;
+		_Charc?: TCharcEntity[];
+	};
+	export type TCharcEntity = {
+		AdditionalValueIsAllowed: boolean;
+		Characteristic: string;
+		Characteristic_Text: string;
+		CharcDataType: CharacteristicDataType;
+		CharcDecimals: number;
+		CharcExponentFormat: string;
+		CharcExponentValue: number;
+		CharcInternalID: string;
+		TimeIntervalNumber: string;
+		CharcLength: number;
+		CharcTemplate: string;
+		CharcValueUnit: string;
+		Currency: string;
+		EntryIsRequired: boolean;
+		MultipleValuesAreAllowed: boolean;
+		NegativeValueIsAllowed: boolean;
+		ValueIntervalIsAllowed: boolean;
+		ValueIsCaseSensitive: boolean;
+		_Values?: TCharcValEntity[];
+		_ClassCharc?: TClassCharcEntity;
+	};
+	export type TCharcValEntity = {
+		CharcInternalID: string;
+		CharcValuePositionNumber: number;
+		TimeIntervalNumber: number;
+		CharcValueParentPositionNumber: number;
+		CharcValueHasChild: boolean;
+		CharcValueIntervalType: string;
+		CharcValue: string;
+		CharcFromNumericValue: number;
+		CharcToNumericValue: number;
+		CharcFromNumericValueUnit: string;
+		CharcToNumericValueUnit: string;
+		CharcFromDate: string;
+		CharcToDate: string;
+		CharcFromTime: string;
+		CharcToTime: string;
+		CharcFromAmount: number;
+		CharcToAmount: number;
+		Currency: string;
+		IsDefaultValue: boolean;
+		CharcFromDecimalValue: string;
+		CharcToDecimalValue: string;
+		CharcValueDescription: string;
+	};
+	export type TEquipmentTextEntity = {
+		Equipment: string;
+		Language: string;
+		EquipmentName: string;
+	};
+	export type TEquipmentHierarchyCustomEntity = {
+		Equipment: string;
+		SuperordinateEquipment: string;
+		HierarchyRoot: string;
+		HierarchyLevel: number;
+		_EquipmentText?: TEquipmentTextEntity;
+		_Root?: TRootEntity[];
+	};
+	export type TDocumentCreateItemEntity = {
+		DocumentItemUUID?: string;
+		DocumentHeaderUUID?: string;
+		SalesDocument?: string;
+		SalesDocumentItem?: number;
+		HigherLevelItem?: number;
+		Material: string;
+		Equipment?: string;
+		Plant: string;
+		StorageLocation: string;
+		SerialNumber?: string;
+		TargetQuantity?: string;
+		TargetQuantityUnit?: string;
+	};
+}
 declare module "vss/com/rcl/model/type/CharacteristicValues" {
 	import Raw from "sap/ui/model/odata/type/Raw";
 	/**
 	 * @namespace vss.com.rcl.model.type
 	 */
 	export default class CharacteristicValues extends Raw {
-		private _typeDecimal;
 		constructor(oFormatOptions?: object, oConstraints?: object);
 		formatValue(vValue: any, sTargetType: string): any;
 		getName(): string;
@@ -45,6 +232,8 @@ declare module "vss/com/rcl/cart/ICartModel" {
 		CartFull = "sap-icon://cart-full"
 	}
 	export type TContractItemData = {
+		SalesDocumentItem?: number;
+		HigherLevelItem?: number;
 		Equipment: string;
 		Equipment_Text?: string;
 		SerialNumber: string;
@@ -108,100 +297,22 @@ declare module "vss/com/rcl/cc/Component" {
 		}>;
 	}
 }
-declare module "vss/com/rcl/cc/model/Enums" {
-	export enum FixedString {
-		eol = "\n\r"
-	}
-	export enum ModelName {
-		action = "action",
-		cart = "cart",
-		device = "device",
-		i18n = "i18n",
-		local = "local",
-		ui = "ui",
-		pageInternal = "pageInternal",
-		internal = "internal"
-	}
-	export enum RouteName {
-		ContractList = "ContractList",
-		ContractObjectPage = "ContractObjectPage",
-		ContractItemObjectPageOV = "ContractItemObjectPageOV",
-		ContractItemObjectPageLG = "ContractItemObjectPageLG",
-		ContractItemObjectPagePG = "ContractItemObjectPagePG",
-		ContractItemObjectPagePR = "ContractItemObjectPagePR"
-	}
-	export enum DataSourceName {
-		mainService = "mainService"
-	}
-	export enum ControlId {
-		/**
-		 * @link https://sapui5.hana.ondemand.com/1.96.1/resources/sap/fe/templates/ObjectPage/view/fragments/FooterContent.fragment.xml
-		 */
-		cancelButton = "fe::FooterBar::StandardAction::Cancel",
-		saveButton = "fe::FooterBar::StandardAction::Save",
-		contractItemsTable = "fe::table::_Items::LineItem"
-	}
-	export enum VSSEventId {
-		BillingCancel = "BILLING_CANCEL",
-		BillingCreate = "BILLING_CREATE"
-	}
-	export enum HTTPMethod {
-		GET = "GET",
-		PUT = "PUT",
-		POST = "POST",
-		DELETE = "DELETE"
-	}
-	export enum EditMode {
-		Editable = "Editable"
-	}
-	export enum RAPAction {
-		Prepare = "com.sap.gateway.srvd.pacg.rso_cm.v0001.Prepare"
-	}
-	export enum EntitySet {
-		ItemGroup = "ItemGroup",
-		ItemGroupText = "ItemGroupText",
-		ItemGroupCategory = "ItemGroupCategory"
-	}
-	export enum UnitOfMeasure {
-		Piece = "PC"
-	}
-}
-declare module "vss/com/rcl/cc/model/InvokeActionPrepare" {
-	import { IObjectPageExtensionAPI } from "vss/com/fe/ObjectPage";
+declare module "vss/com/rcl/utils/FioriTable" {
+	import ResponsiveTable from "sap/m/Table";
+	import Control from "sap/ui/core/Control";
+	import Table from "sap/ui/table/Table";
+	import { IFilterConditionsAware } from "vss/com/fe/ListReport";
 	/**
-	 * invoke validation
-	 * @link https://help.sap.com/docs/ABAP_PLATFORM_2020/fc4c71aa50014fd1b43721701471913d/707c7e93ecab48ea9e6b9c3c893a792e.html?locale=en-US
-	 *
-	 * @namespace vss.com.rcl.cc.model
+	 * @type {sap.ui.mdc.Table}
 	 */
-	export default class InvokeActionPrepare {
-		private _api;
-		private _context;
-		constructor(_api: IObjectPageExtensionAPI);
-		invoke(): Promise<void>;
+	export interface IFioriTable extends IFilterConditionsAware, Control {
+		_oTable: ResponsiveTable | Table;
+		initialized(): Promise<this>;
 	}
-}
-declare module "vss/com/rcl/cc/model/ContractCreateCommand" {
-	import { TDraftAdministrativeData } from "vss/com/fe/DraftTypes";
-	type TDocCreateOut = void;
-	/**
-	 * @namespace vss.com.rcl.cc.model
-	 */
-	export default class ContractCreateCommand {
-		private _model;
-		constructor();
-		call(draftAdministrativeData: TDraftAdministrativeData): Promise<TDocCreateOut>;
-		messageDialog(): import("sap/m/Dialog").default | undefined;
-	}
-}
-declare module "vss/com/rcl/utils/Logger" {
-	/**
-	 * @namespace vss.com.rcl.utils
-	 */
-	export function debug(message: string): void;
-	export function error(message: string): void;
-	export function info(message: string): void;
-	export function warning(message: string): void;
+	export function getResponsiveTable(table: IFioriTable): ResponsiveTable;
+	export function getTable(table: IFioriTable): Table;
+	export function getResponsiveTablePromise(table: IFioriTable): Promise<ResponsiveTable>;
+	export function getTablePromise(table: IFioriTable): Promise<Table>;
 }
 declare module "vss/com/rcl/utils/Container" {
 	import { IListReportExtensionAPI, IFilterBar } from "vss/com/fe/ListReport";
@@ -247,6 +358,71 @@ declare module "vss/com/rcl/utils/Container" {
 		set actionDropdown(control: MenuButton);
 	}
 }
+declare module "vss/com/rcl/cc/model/CartItemsToContract" {
+	import ODataListBinding from "sap/ui/model/odata/v4/ODataListBinding";
+	import { IObjectPageExtensionAPI } from "vss/com/fe/ObjectPage";
+	import { TCartItem } from "vss/com/rcl/cart/ICartModel";
+	/**
+	 * @namespace vss.com.rcl.cc.model
+	 */
+	export default class CartItemsToContract {
+		private _api;
+		private _itemsBinding;
+		constructor(_api: IObjectPageExtensionAPI, _itemsBinding: ODataListBinding);
+		create(...cartItems: TCartItem[]): Promise<unknown>;
+	}
+}
+declare module "vss/com/rcl/cc/eh/ContractDraftCreateHandler" {
+	import { IObjectPageController } from "vss/com/fe/ObjectPage";
+	/**
+	 * - Validate Contract data
+	 * - Propagate CART items into CreateContract entity
+	 *
+	 * @namespace vss.com.rcl.cc.eh
+	 */
+	export default class ContractDraftCreateHandler {
+		private _ctrl;
+		constructor(_ctrl: IObjectPageController);
+		private run;
+	}
+}
+declare module "vss/com/rcl/cc/model/InvokeActionPrepare" {
+	import { IObjectPageExtensionAPI } from "vss/com/fe/ObjectPage";
+	/**
+	 * invoke validation
+	 * @link https://help.sap.com/docs/ABAP_PLATFORM_2020/fc4c71aa50014fd1b43721701471913d/707c7e93ecab48ea9e6b9c3c893a792e.html?locale=en-US
+	 *
+	 * @namespace vss.com.rcl.cc.model
+	 */
+	export default class InvokeActionPrepare {
+		private _api;
+		private _context;
+		constructor(_api: IObjectPageExtensionAPI);
+		invoke(): Promise<void>;
+	}
+}
+declare module "vss/com/rcl/cc/model/ContractCreateCommand" {
+	import { TDraftAdministrativeData } from "vss/com/fe/DraftTypes";
+	type TDocCreateOut = void;
+	/**
+	 * @namespace vss.com.rcl.cc.model
+	 */
+	export default class ContractCreateCommand {
+		private _model;
+		constructor();
+		call(draftAdministrativeData: TDraftAdministrativeData): Promise<TDocCreateOut>;
+		messageDialog(): import("sap/m/Dialog").default | undefined;
+	}
+}
+declare module "vss/com/rcl/utils/Logger" {
+	/**
+	 * @namespace vss.com.rcl.utils
+	 */
+	export function debug(message: string): void;
+	export function error(message: string): void;
+	export function info(message: string): void;
+	export function warning(message: string): void;
+}
 declare module "vss/com/rcl/cc/eh/SaveHandler" {
 	import { IObjectPageController } from "vss/com/fe/ObjectPage";
 	import Event from "sap/ui/base/Event";
@@ -255,20 +431,6 @@ declare module "vss/com/rcl/cc/eh/SaveHandler" {
 	 */
 	export default class SaveHandler {
 		static onSave(this: IObjectPageController, event: Event): void;
-	}
-}
-declare module "vss/com/rcl/cc/ext/CartItemsToContract" {
-	import ODataListBinding from "sap/ui/model/odata/v4/ODataListBinding";
-	import { IObjectPageExtensionAPI } from "vss/com/fe/ObjectPage";
-	import { TCartItem } from "vss/com/rcl/cart/ICartModel";
-	/**
-	 * @namespace vss.com.rcl.cc.ext
-	 */
-	export default class CartItemsToContract {
-		private _api;
-		private _itemsBinding;
-		constructor(_api: IObjectPageExtensionAPI, _itemsBinding: ODataListBinding);
-		create(...cartItems: TCartItem[]): Promise<unknown>;
 	}
 }
 declare module "vss/com/rcl/cc/ext/OverrideSaveButton" {
@@ -287,31 +449,12 @@ declare module "vss/com/rcl/cc/ext/OverrideSaveButton" {
 		private createSaveButton;
 	}
 }
-declare module "vss/com/rcl/utils/FioriTable" {
-	import ResponsiveTable from "sap/m/Table";
-	import Control from "sap/ui/core/Control";
-	import Table from "sap/ui/table/Table";
-	import { IFilterConditionsAware } from "vss/com/fe/ListReport";
-	/**
-	 * @type {sap.ui.mdc.Table}
-	 */
-	export interface IFioriTable extends IFilterConditionsAware, Control {
-		_oTable: ResponsiveTable | Table;
-		initialized(): Promise<this>;
-	}
-	export function getResponsiveTable(table: IFioriTable): ResponsiveTable;
-	export function getTable(table: IFioriTable): Table;
-}
 declare module "vss/com/rcl/cc/ext/controller/ObjectPageExtension.controller" {
 	import { IObjectPageController } from "vss/com/fe/ObjectPage";
 	const override: {
 		onInit(this: IObjectPageController): void;
 		onBeforeRendering(this: IObjectPageController, event: Event): void;
 		onAfterRendering(this: IObjectPageController, event: Event): void;
-		/**
-		 * - Validate Contract data
-		 * - Propagate CART items into CreateContract entity
-		 */
 		onPageReady(this: IObjectPageController, state: any): void;
 		onPendingFilters(this: IObjectPageController, event: Event): void;
 		onViewNeedsRefresh(this: IObjectPageController, event: Event): void;
@@ -398,96 +541,6 @@ declare module "vss/com/rcl/ml/eh/CartItemEventHandler" {
 		static onActionButtonAddToCartPressed(this: IListReportExtensionAPI | IObjectPageController, event: Event): void;
 	}
 }
-declare module "vss/com/rcl/types/EntitySet" {
-	import { CharacteristicDataType, RentalClassType } from "vss/com/rcl/library";
-	export type TRootEntity = {
-		Equipment: string;
-		EquipmentName: string;
-		Material: string;
-		MaterialName: string;
-		Plant: string;
-		PlantName: string;
-		StorageLocation: string;
-		StorageLocationName: string;
-		RentalClassType: string;
-		RentalClassTypeText: string;
-		HierarchyLevel: number;
-		HierarchyRoot: string;
-		SuperordinateEquipment: string;
-		SuperordinateEquipmentName: string;
-	};
-	export type TCharcFilterEntity = {
-		CharcInternalID: string;
-		_Charc?: TCharcEntity;
-		_ClassCharc?: TClassCharcEntity[];
-	};
-	export type TClassCharcEntity = {
-		CharcInternalID: string;
-		ClassInternalID: string;
-		ClassDescription: string;
-		RentalClassType: RentalClassType;
-		_Charc?: TCharcEntity[];
-	};
-	export type TCharcEntity = {
-		AdditionalValueIsAllowed: boolean;
-		Characteristic: string;
-		Characteristic_Text: string;
-		CharcDataType: CharacteristicDataType;
-		CharcDecimals: number;
-		CharcExponentFormat: string;
-		CharcExponentValue: number;
-		CharcInternalID: string;
-		TimeIntervalNumber: string;
-		CharcLength: number;
-		CharcTemplate: string;
-		CharcValueUnit: string;
-		Currency: string;
-		EntryIsRequired: boolean;
-		MultipleValuesAreAllowed: boolean;
-		NegativeValueIsAllowed: boolean;
-		ValueIntervalIsAllowed: boolean;
-		ValueIsCaseSensitive: boolean;
-		_Values?: TCharcValEntity[];
-		_ClassCharc?: TClassCharcEntity;
-	};
-	export type TCharcValEntity = {
-		CharcInternalID: string;
-		CharcValuePositionNumber: number;
-		TimeIntervalNumber: number;
-		CharcValueParentPositionNumber: number;
-		CharcValueHasChild: boolean;
-		CharcValueIntervalType: string;
-		CharcValue: string;
-		CharcFromNumericValue: number;
-		CharcToNumericValue: number;
-		CharcFromNumericValueUnit: string;
-		CharcToNumericValueUnit: string;
-		CharcFromDate: string;
-		CharcToDate: string;
-		CharcFromTime: string;
-		CharcToTime: string;
-		CharcFromAmount: number;
-		CharcToAmount: number;
-		Currency: string;
-		IsDefaultValue: boolean;
-		CharcFromDecimalValue: string;
-		CharcToDecimalValue: string;
-		CharcValueDescription: string;
-	};
-	export type TEquipmentTextEntity = {
-		Equipment: string;
-		Language: string;
-		EquipmentName: string;
-	};
-	export type TEquipmentHierarchyCustomEntity = {
-		Equipment: string;
-		SuperordinateEquipment: string;
-		HierarchyRoot: string;
-		HierarchyLevel: number;
-		_EquipmentText?: TEquipmentTextEntity;
-		_Root?: TRootEntity[];
-	};
-}
 declare module "vss/com/rcl/model/EquipmentHierarchyQuery" {
 	import ODataModel from "sap/ui/model/odata/v4/ODataModel";
 	import { TEquipmentHierarchyCustomEntity, TRootEntity } from "vss/com/rcl/types/EntitySet";
@@ -563,7 +616,8 @@ declare module "vss/com/rcl/ml/ext/ObjectPageSectionExtension" {
 		private _pageLayout;
 		constructor(_api: IObjectPageExtensionAPI);
 		private bindAccessoriesVisibility;
-		private applyEquipmentHierarchyFilter;
+		private loadEquipmentHierarchy;
+		private tree;
 		private section;
 		private data;
 	}
