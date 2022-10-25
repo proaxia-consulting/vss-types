3b883a313b465608e8405d94f6cd3bb73db0a175
Marek Gozdalski <marek.gozdalski@gmail.com>
2022-10-25 11:59:48 +0200

    remove srs lib

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,6 +1,5 @@
 /**
- * DBM|E & VSS TypeScript Definitions
+ * VSS TypeScript Definitions
  */
-/// <reference path="./dbme.w.lib.core.d.ts"/>
 /// <reference path="./dbme.f.timeclocking.d.ts"/>
 /// <reference path="./vss.com.fe.d.ts"/>
