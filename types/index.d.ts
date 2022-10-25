f9c33e7b5709162ae51c9ae3ff385be887f4c71e
Marek Gozdalski <marek.gozdalski@gmail.com>
2022-10-25 09:50:03 +0200

    remove existing packages

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,8 +1,6 @@
 /**
  * DBM|E & VSS TypeScript Definitions
  */
-/// <reference path="./dbme.c.d.ts"/>
 /// <reference path="./dbme.w.lib.core.d.ts"/>
 /// <reference path="./dbme.f.timeclocking.d.ts"/>
-/// <reference path="./dbme.srs.com.tdo.d.ts"/>
 /// <reference path="./vss.com.fe.d.ts"/>
