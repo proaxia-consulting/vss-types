2c211e03aa95635c5818c537a9fd3ba0a3527601
Marek Gozdalski <marek.gozdalski@gmail.com>
2023-01-25 19:02:05 +0100

    sap/ui/mdc/field

--- types/sap.ui.mdc.d.ts
+++ types/sap.ui.mdc.d.ts
@@ -35,3 +35,76 @@ declare module "sap/ui/mdc/ActionToolbar" {
 		getActions(): ActionToolbarAction[];
 	}
 }
+
+declare module "sap/ui/mdc/field/FieldInput" {
+	import Input from "sap/m/Input";
+	import Field from "sap/ui/mdc/Field";
+
+	export default class FieldInput extends Input {
+		getParent(): Field;
+	}
+}
+
+declare module "sap/ui/mdc/field/FieldMultiInput" {
+	import MultiInput from "sap/m/MultiInput";
+	import MultiValueField from "sap/ui/mdc/MultiValueField";
+
+	export default class FieldMultiInput extends MultiInput {
+		getParent(): MultiValueField;
+	}
+}
+
+declare module "sap/ui/mdc/Control" {
+	import CoreControl from "sap/ui/core/Control";
+	export default class Control extends CoreControl {}
+}
+
+declare module "sap/ui/mdc/field/FieldBase" {
+	import Control from "sap/ui/mdc/Control";
+	import InputBase from "sap/m/InputBase";
+	import Event from "sap/ui/base/Event";
+
+	export default class FieldBase extends Control {
+		_oCreateContentPromise: Promise<InputBase> | undefined;
+
+		attachSubmit(handler: (event: Event) => void, listener?: object): void;
+		getConditions(): object[];
+		setMaxConditions(max: int): void;
+		getMaxConditions(): int;
+		getLabel(): string;
+		setLabel(label: string): void;
+	}
+}
+
+declare module "sap/ui/mdc/Field" {
+	import FieldBase from "sap/ui/mdc/field/FieldBase";
+	import Event from "sap/ui/base/Event";
+
+	export default class Field extends FieldBase {
+		attachChange(handler: (event: Event) => void, listener?: object): void;
+		setValue(value: unknown): void;
+		getValue(): unknown;
+		setAdditionalValue(value: unknown): void;
+		getAdditionalValue(): unknown;
+	}
+}
+
+declare module "sap/ui/mdc/field/MultiValueFieldItem" {
+	import Element from "sap/ui/core/Element";
+
+	export default class MultiValueFieldItem extends Element {
+		getKey(): unknown;
+		setKey(key: unknown): void;
+		getDescription(): unknown;
+		setDescription(key: unknown): void;
+	}
+}
+
+declare module "sap/ui/mdc/MultiValueField" {
+	import FieldBase from "sap/ui/mdc/field/FieldBase";
+	import MultiValueFieldItem from "sap/ui/mdc/field/MultiValueFieldItem";
+
+	export default class MultiValueField extends FieldBase {
+		getItems(): MultiValueFieldItem[];
+	}
+}
