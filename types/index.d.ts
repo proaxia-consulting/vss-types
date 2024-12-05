53c68584e20926b7fddae57111eacee3415d24b4
Marek Gozdalski <marek.gozdalski@proaxia-consulting.com>
2024-12-05 17:14:38 +0100

    include dbme.c

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,6 +2,7 @@
  * VSS TypeScript Definitions
  */
 /// <reference path="./ui5.d.ts"/>
+/// <reference path="./dbme.c.d.ts"/>
 /// <reference path="./dbme.f.timeclocking.d.ts"/>
 /// <reference path="./vss.com.fe.d.ts"/>
 /// <reference path="./sap.ui.mdc.d.ts"/>
