d8b6babd56f12f9888bd3124fad2c9dfffbe6393
Marek Gozdalski <marek.gozdalski@proaxia-consulting.com>
2025-07-01 19:07:27 +0200

    Update generated types

---
+++ types/index.d.ts
@@ -0,0 +1,6 @@
+/* eslint-disable @typescript-eslint/triple-slash-reference */
+/**
+ * VSS TypeScript Definitions
+ */
+/// <reference path="./ui5.d.ts"/>
+/// <reference path="./vss.com.fe.d.ts"/>
