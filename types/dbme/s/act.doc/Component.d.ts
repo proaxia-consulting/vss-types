166a60d419fbc2535ffacd0c72a61ac9b177eda3
Marek Gozdalski <marek.gozdalski@proaxia-consulting.com>
2026-08-06 15:59:53 +0200

    Remove dbme/s/act.doc

--- types/dbme/s/act.doc/Component.d.ts
+++
@@ -1,9 +0,0 @@
-import UIComponent from "sap/ui/core/UIComponent";
-/**
- * @namespace dbme.s.act.doc
- */
-export default class Component extends UIComponent {
-    static readonly metadata: object;
-    init(): void;
-    private _initTranslate;
-}
