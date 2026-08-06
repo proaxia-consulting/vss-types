001dd26429d1cc8b86670499fa01c18a02e6e694
Marek Gozdalski <marek.gozdalski@proaxia-consulting.com>
2026-08-06 15:57:43 +0200

    Update generated types

---
+++ types/dbme/s/act/doc/lib/Constants.d.ts
@@ -0,0 +1,12 @@
+declare const Constants: {
+    readonly Itmnr: {
+        readonly Initial: "000000";
+    };
+    readonly ActionLevel: {
+        readonly Item: "I";
+    };
+    readonly Docnr: {
+        readonly Dummy: "$000000001";
+    };
+};
+export default Constants;
